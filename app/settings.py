73be07e75a42efb6b6300a634ff9d976d9c2d544
Saul Meireles <saulm@live.com>
2024-10-31 00:09:15 -0300

    config allowed_hosts

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-jnu!r%jye8*wroe24r0luc6-p#3yiwae3d%21c51ta54$^4nt^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
